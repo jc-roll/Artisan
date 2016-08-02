833327b0549a0419aafcce6fc27877dce3b06be6
James Roll <jcroll@fullsail.edu>
2016-08-02 09:16:36 -0400

    Done with fb login for now makign registration

--- public/js/controllers/login.js
+++ public/js/controllers/login.js
@@ -13,48 +13,34 @@ myApp.controller('LoginController',
 
 ///////////////////////////////////////////Facebook Login Options///////////////////////////////////////////////////
   
-// This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    };
-  };
+  $scope.watchLoginChange = function() {
+    var _self = this;
+    FB.Event.subscribe('auth.authResponseChange', function(res) {
+      if (res.status === 'connected') {
+        _self.getUserInfo();
+        }
+        else {
+        }
+      });
+    }
 
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
+  $scope.getUserInfo = function() {
+    var _self = this;
+    FB.api('/me', function(res) {
+      $rootScope.$apply(function() {
+        $rootScope.user = _self.user = res;
+      });
     });
   }
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'You Logged In, ' + response.name + '!';
-          
+  $scope.logout = function() {
+    var _self = this;
+    FB.logout(function(response) {
+      $rootScope.$apply(function() {
+        $rootScope.user = _self.user = {};
+      });
     });
   }
-
-
 
   }]);
 
