76332ef70de00233d86526f1c41fcc1e9e22269d
James Roll <jcroll@fullsail.edu>
2016-08-02 08:58:45 -0400

    Was using javascript and not implementing Angular...starting Angular

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,67 @@ var myApp = angular.module('myApp',
 
 
 
-myApp.run(['$rootScope', '$location',
-  function($rootScope, $location) {
-    $rootScope.$on('$routeChangeError',
-      function(event, next, previous, error) {
-        if (error=='AUTH_REQUIRED') {
-          $rootScope.message = 'Sorry, you must log in to access that page';
-          $location.path('/home');
-        } // AUTH REQUIRED
-      }); //event info
-  }]); //run
+myApp.run(['$rootScope', '$location', '$window', 'srvAuth',
+  function($rootScope, $location, $window, sAuth) {
+    $rootScope.user = {};
+    $window.fbAsyncInit = function() {
+      // Executed when the SDK is loaded
+      FB.init({
+        /*
+         The app id of the web app;
+         To register a new app visit Facebook App Dashboard
+         ( https://developers.facebook.com/apps/ )
+        */
+        appId: '1812013779036047',
+        /*
+         Adding a Channel File improves the performance
+         of the javascript SDK, by addressing issues
+         with cross-domain communication in certain browsers.
+        */
+        channelUrl: 'app/channel.html',
+        /*
+         Set if you want to check the authentication status
+         at the start up of the app
+        */
+        status: true,
+        /*
+         Enable cookies to allow the server to access
+         the session
+        */
+        cookie: true,
+        /* Parse XFBML */
+        xfbml: true
+      });
+      sAuth.watchAuthenticationStatusChange();
+    };
 
+  (function(d){
+    // load the Facebook javascript SDK
+    var js,
+    id = 'facebook-jssdk',
+    ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
 
+    ref.parentNode.insertBefore(js, ref);
+
+  }(document));
+
+}]);
 
 myApp.config(['$routeProvider', function($routeProvider) {
+    var config = {
+    apiKey: "AIzaSyBapIsFvALXS5Ysgdepi8iql2QNCxDwOFQ",
+    authDomain: "finalproject-33408.firebaseapp.com",
+    databaseURL: "https://finalproject-33408.firebaseio.com/",
+    storageBucket: "finalproject-33408.appspot.com",
+  };
+  firebase.initializeApp(config);
   $routeProvider.
     when('/login', {
       templateUrl: 'views/login.html',
--- public/js/controllers/login.js
+++ public/js/controllers/login.js
@@ -33,52 +33,14 @@ myApp.controller('LoginController',
       // they are logged into this app or not.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into Facebook.';
-    }
-  }
+    };
+  };
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
   function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
     });
   }
-
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1812013779036047',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.5' // use graph api version 2.5
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
@@ -87,31 +49,12 @@ myApp.controller('LoginController',
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+        'You Logged In, ' + response.name + '!';
           
     });
   }
 
 
-
-  function registerFB(response) {
-      auth.$createUser({
-        email: response.email
-      }).then(function(regUser) {
-
-    var regRef = new Firebase(FIREBASE_URL + 'users/'+regUser.uid)
-    var firebaseUser = $firebaseObject(regRef);
-        firebaseUser.firstname = response.firstname;
-        firebaseUser.lastname = response.lastname;
-        firebaseUser.email = response.email;
-        firebaseUser.date = response.ServerValue.TIMESTAMP;
-        firebaseUser.$save();
-        console.log('Logged Data');
-
-      }).catch(function(error) {
-        $rootScope.message = error.message;
-      }); // //createUser
-    } // register
 
   }]);
 
