8b8ae4a268807c6e352079d27287a4b5b08333f7
James Roll <jcroll@fullsail.edu>
2016-08-09 01:24:55 -0400

    Working on file structure

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,22 @@
-var app = angular.module('myApp', ['ngRoute', 'firebase']);
+var myApp = angular.module('myApp', ['ngRoute', 'firebase']);
 
 // for ngRoute
-app.run(["$rootScope", "$location", function($rootScope, $location) {
+myApp.run(["$rootScope", "$location", function($rootScope, $location) {
   $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
     // We can catch the error thrown when the $requireSignIn promise is rejected
     // and redirect the user back to the home page
     console.log(error);
     if (error === "AUTH_REQUIRED") {
-      $location.path("/login");
+      $location.path("/home");
     }
   });
 }]);
 
-app.config(["$routeProvider", function($routeProvider) {
+myApp.config(["$routeProvider", function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'views/landing.html',
+    controller: 'AuthCtrl'
+  })
   $routeProvider.when("/login", {
     // the rest is the same for ui-router and ngRoute...
     controller: "LoginCtrl",
@@ -68,19 +72,195 @@ app.config(["$routeProvider", function($routeProvider) {
 }]);
 
 
-app.controller('RegisterCtrl', ['$scope', function($scope) {
+myApp.controller('RegisterCtrl', ['$scope', function($scope) {
 
     $scope.submit = function() {
         firebase.auth().createUserWithEmailAndPassword($scope.email, $scope.password).then(function(status) {
                 firebase.database().ref('users/').set({
-                    firebaseUser.name: $scope.name,
-                    firebaseUser.email: $scope.email
+                    name: $scope.name,
+                    email: $scope.email,
+                    uid: $scope.uid
             })
           })
         }
       }]);
 
-app.controller("LoginCtrl", ["currentAuth", "$scope", "Auth", "$route", function(currentAuth, $scope, Auth, $route) {
+myApp.controller('AuthCtrl', ['$scope', '$timeout', '$window', '$location', '$cookies', '$firebaseObject', '$firebaseArray', '$firebaseAuth', function($scope, $timeout, $window, $location, $cookies, $firebaseObject, $firebaseArray, $firebaseAuth) {
+
+    // facebook sdk for grab data from Facebook prfile
+    $window.fbAsyncInit = function() {
+        FB.init({
+          appId: '294798217536135',
+          status: true,
+          cookie: true,
+          xfbml: true,
+          version: 'v2.6'
+        });
+    };
+
+    /*------------------------------------*\
+        AUTH
+    \*------------------------------------*/
+
+    // 1. Auth with pass and email
+    $scope.submit = function() {
+        // $scope.email = 'lollipop.fly@gmail.com';
+        // $scope.password = 'kerbalcrasauders1';
+
+        firebase.auth().signInWithEmailAndPassword($scope.email, $scope.password).then(function(user) {
+            console.log(user);
+        }).catch(function(error) {
+          // Handle Errors here.
+          $scope.$applyAsync(function() {
+            $scope.errorMessage = error.message;
+          });
+        });
+    };
+
+    // 2. Auth Facebook
+    var provider = new firebase.auth.FacebookAuthProvider();
+    // Permissions
+    provider.addScope('email');
+    provider.addScope('user_friends');
+
+    $scope.facebook = function () {
+        // firebase.auth().signInWithRedirect(provider);
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          console.log(token);
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+    };
+
+    // Log Out
+    $scope.logOut = function () {
+        firebase.auth().signOut().then(function() {
+            console.log('Sign-out successful.');
+        }, function(error) {
+            console.log('An error happened.');
+        });
+    };
+
+    // If user Logged in v1
+    // $timeout(function () {
+    //     $scope.user = firebase.auth().currentUser;
+    //     if($scope.user) {
+    //         $scope.uid = $scope.user.uid
+    //         $scope.getUsers();
+    //     }
+    // console.log($scope.user);
+    //     }, 200);
+
+    // If user Logged in v2
+    // firebase.auth().onAuthStateChanged(function(user) {
+    //   if (user) {
+    //     $scope.uid = user.uid;
+    //     console.log($scope.uid);
+    //     // User is signed in.
+    //     console.log('User is signed in.');
+    //     $scope.getUsers();
+    //     // console.log(firebase.auth().currentUser);
+    //   } else {
+    //     // No user is signed in.
+    //     console.log('No user is signed in.');
+    //   }
+    // });
+
+    /*------------------------------------*\
+        DATA
+    \*------------------------------------*/
+
+
+    // Get Data from collection json
+    $scope.getData = function() {
+      var ref = firebase.database().ref('collection/');
+      $scope.data = $firebaseArray(ref);
+      console.log($scope.data);
+    };
+
+
+    // Get users form users json
+    $scope.getUsers = function() {
+        var ref = firebase.database().ref('users/'+ $scope.uid);
+
+        $scope.users = $firebaseArray(ref);
+        console.log($firebaseArray(ref));
+    };
+
+
+
+    // Add new Data to Users (Перезаписывает весь json users)
+    $scope.setUser = function() {
+        firebase.database().ref('users/').set({
+            name: $scope.userName,
+            email: $scope.userEmail,
+            uid: $scope.uid
+        });
+    };
+
+    // Add new Data to Users (добавляет новый json users)
+    $scope.pushUser = function() {
+        firebase.database().ref('users/'+$scope.uid).push({
+            name: $scope.userName,
+            email: $scope.userEmail,
+            uid: $scope.uid
+        }).catch(function (error) {
+            console.log(error);
+        });
+    };
+
+
+    // UPDATE
+    $scope.updateUser = function(user) {
+        var postData = {
+            name: 'updateName',
+            email: 'updatedEmail'
+          };
+          // var key = firebase.database().ref().child('users').push().key
+          firebase.database().ref('users/'+ $scope.uid +"/" + user.$id).update(postData);
+        // firebase.database().ref('users/').update({
+        //     name: $scope.userName,
+        //     email: $scope.userEmail
+        // });
+    };
+
+    // DELETE
+    $scope.deleteUser = function(user) {
+        // $scope.users.delete(user);
+        firebase.database().ref('users/'+ $scope.uid +"/" + user.$id).remove();
+    };
+
+
+    /*------------------------------------*\
+        Facebook SDK
+    \*------------------------------------*/
+
+    $scope.getFacebookInfo = function () {
+        FB.api(
+           "/me",
+            function (response) {
+                console.log(response);
+              if (response && !response.error) {
+                /* handle the result */
+              }
+            }
+        );
+    };
+}]);
+
+myApp.controller("LoginCtrl", ["currentAuth", "$scope", "Auth", "$route", function(currentAuth, $scope, Auth, $route) {
   // currentAuth (provided by resolve) will contain the
   // authenticated user or null if not signed in
   $scope.auth = Auth;
@@ -108,7 +288,7 @@ app.controller("LoginCtrl", ["currentAuth", "$scope", "Auth", "$route", function
   //console.log(Auth.$requireSignIn());
 }]);
 
-app.controller("HomeCtrl", ["currentAuth", "$scope", "Auth", "$route", function(currentAuth, $scope, Auth, $route) {
+myApp.controller("HomeCtrl", ["currentAuth", "$scope", "Auth", "$route", function(currentAuth, $scope, Auth, $route) {
   // currentAuth (provided by resolve) will contain the
   // authenticated user or null if not signed in
 
@@ -132,7 +312,7 @@ app.controller("HomeCtrl", ["currentAuth", "$scope", "Auth", "$route", function(
   //console.log(Auth.$requireSignIn());
 }]);
 
-app.controller("AccountCtrl", ["currentAuth", "$scope", "Auth", "$route", function(currentAuth, $scope, Auth, $route) {
+myApp.controller("AccountCtrl", ["currentAuth", "$scope", "Auth", "$route", function(currentAuth, $scope, Auth, $route) {
   // currentAuth (provided by resolve) will contain the
   // authenticated user or null if not signed in
 
@@ -156,7 +336,7 @@ app.controller("AccountCtrl", ["currentAuth", "$scope", "Auth", "$route", functi
 }]);
 
 // let's create a re-usable factory that generates the $firebaseAuth instance
-app.factory("Auth", ["$firebaseAuth",
+myApp.factory("Auth", ["$firebaseAuth",
   function($firebaseAuth) {
     return $firebaseAuth();
   }
