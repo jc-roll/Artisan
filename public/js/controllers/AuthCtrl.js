944ca1721d2f39a9bd21862e7f87677ab6849e06
James Roll <jcroll@fullsail.edu>
2016-08-27 14:01:19 -0400

    Built facebook factory and scope calls

--- public/js/controllers/AuthCtrl.js
+++ public/js/controllers/AuthCtrl.js
@@ -11,7 +11,7 @@ myApp.controller('AuthCtrl', ['$scope', '$rootScope', '$timeout', '$window', '$l
         var getData = $firebaseObject(userData);
         $rootScope.currentUser = getData;
     } else {
-      console.log("Signed out");
+      console.log(" Currently signed out of site login");
 
     }
   });
@@ -59,40 +59,149 @@ myApp.controller('AuthCtrl', ['$scope', '$rootScope', '$timeout', '$window', '$l
   }
 
 
-  $scope.facebook = function() {
-    $scope.authObj.$signInWithPopup("facebook").then(function(result) {
-      console.log('Welcome!  Fetching your information.... ');
-      console.log("Signed in as:", result.user.uid);
-    }).catch(function(error) {
-      console.error("Authentication failed:", error);
-    });
-  }
-
 }]);  
 
-  // This is called with the results from from FB.getLoginStatus().
-    function statusChangeCallback(response) {
-      console.log('statusChangeCallback');
-      console.log(response);
-      // The response object is returned with a status field that lets the
-      // app know the current login status of the person.
-      // Full docs on the response object can be found in the documentation
-      // for FB.getLoginStatus().
-      if (response.status === 'connected') {
+//--------------------- facebook Scope the data ------------------------
+
+$scope.getName = function() {
+   facebookService.getName() 
+     .then(function(response) {
+       $scope.name = response.name;
+     }
+   );
+};
+
+$scope.getLastName = function() {
+   facebookService.getLastName() 
+     .then(function(response) {
+       $scope.last_name = response.last_name;
+     }
+   );
+};
+$scope.getFirstName = function() {
+   facebookService.getFirstName() 
+     .then(function(response) {
+       $scope.first_name = response.first_name;
+     }
+   );
+};
+$scope.getEmail = function() {
+   facebookService.getEmail() 
+     .then(function(response) {
+       $scope.email = response.email;
+     }
+   );
+};
+$scope.getID = function() {
+   facebookService.getID() 
+     .then(function(response) {
+       $scope.id = response.id;
+     }
+   );
+};
+
+
+
+//--------------------- facebook Factory ------------------------
+
+
+
+myApp.factory('facebookService', function($q) {
+    return {
+        getName: function() {
+            var deferred = $q.defer();
+            FB.api('/me', {fields: 'name'}, function(response) {
+                if (!response || response.error) {deferred.reject('Error occured');
+                } else {deferred.resolve(response);
+                }
+            });
+                return deferred.promise;
+        }
+        getFirstName: function() {
+            var deferred = $q.defer();
+            FB.api('/me', {fields: 'first_name'}, function(response) {
+                if (!response || response.error) {deferred.reject('Error occured');
+                } else {deferred.resolve(response);
+                }
+            });
+                return deferred.promise;
+        }
+        getLastName: function() {
+            var deferred = $q.defer();
+            FB.api('/me', {fields: 'last_name'}, function(response) {
+                if (!response || response.error) {deferred.reject('Error occured');
+                } else {deferred.resolve(response);
+                }
+            });
+                return deferred.promise;
+        }
+        getEmail: function() {
+            var deferred = $q.defer();
+            FB.api('/me', {fields: 'email'}, function(response) {
+                if (!response || response.error) {deferred.reject('Error occured');
+                } else {deferred.resolve(response);
+                }
+            });
+                return deferred.promise;
+        }
+        getID: function() {
+            var deferred = $q.defer();
+            FB.api('/me', {fields: 'id'}, function(response) {
+                if (!response || response.error) {deferred.reject('Error occured');
+                } else {deferred.resolve(response);
+                }
+            });
+                return deferred.promise;
+        }
+    }
+});
+//--------------------- facebook key functions ------------------------
+
+
+                    window.fbAsyncInit = function() {
+                      FB.init({
+                        appId      : '1812013779036047',
+                        status     : true, 
+                        cookie     : true,  // enable cookies to allow the server to access 
+                        xfbml      : true,  // parse social plugins on this page
+                        version    : 'v2.5' // use graph api version 2.5
+                      })
+                      FB.getLoginStatus(function(response) {
+                        statusChangeCallback(response);
+                      })
+
+                    }// END OF <WINDOW>
+
+    function facebookLogin() {
+      FB.login(function(response) {
+        var token = response.authResponse.accessToken;
         var uid = response.authResponse.userID;
-        var accessToken = response.authResponse.accessToken;
-        console.log('Good to see you, ' + response.name + '.');
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me', 'get', { access_token: token, fields: 'id,name,gender,email,last_name' }, 
+            function(response) {
+            console.log(response);
+          });
+          } else {
+            console.log('User cancelled login or did not fully authorize.');
+          }
+        });
+      }
+
+    function statusChangeCallback(response) {
+      console.log('Checking facebook status');
+      console.log(response);
+      if (response.status === 'connected') {
       } else if (response.status === 'not_authorized') {
-        // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
           'into this app.';
       } else {
-        // The person is not logged into Facebook, so we're not sure if
-        // they are logged into this app or not.
         document.getElementById('status').innerHTML = 'Please log ' +
           'into Facebook.';
       }
     }
+
+
 
     function checkLoginState() {
       FB.getLoginStatus(function(response) {
@@ -100,14 +209,7 @@ myApp.controller('AuthCtrl', ['$scope', '$rootScope', '$timeout', '$window', '$l
       });
     }
  
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '1812013779036047',
-          cookie     : true,  // enable cookies to allow the server to access 
-          xfbml      : true,  // parse social plugins on this page
-          version    : 'v2.5' // use graph api version 2.5
-        })
-      }
+
      
 
       
