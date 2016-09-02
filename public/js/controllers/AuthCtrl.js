e7fef0f80f84c38dfa40c3ed9c4917d088e536db
James Roll <jcroll@fullsail.edu>
2016-09-02 15:42:10 -0400

    Most styling fixed but need a break finish style and style lize form and buttons next

--- public/js/controllers/AuthCtrl.js
+++ public/js/controllers/AuthCtrl.js
@@ -17,7 +17,6 @@ myApp.controller('AuthCtrl', ['$scope', '$rootScope', '$timeout', '$window', '$l
     }
   });
 
-  
 
   $scope.signinWithFacebook = function(){
         signinWithFacebook.login();
@@ -70,33 +69,31 @@ myApp.controller('AuthCtrl', ['$scope', '$rootScope', '$timeout', '$window', '$l
 }
 
 myApp.factory("signinWithFacebook", function() {
-        var facebookService = [];
-        function writeUserData(userId, name, email) {
-            firebase.database().ref('users/' + userId)
-            .set({
-                username: name,
-                email: email
-            });
-        }
+  var facebookService = [];
+    function writeUserData(userId, name, email) {
+      irebase.database().ref('users/' + userId)
+      .set({
+        username: name,
+        email: email
+    });
+}
 
-  facebookService.login = function() {
-      var facebookProvider = new firebase.auth.FacebookAuthProvider();
-
-          firebase.auth().signInWithPopup(facebookProvider);
-          firebase.auth().onAuthStateChanged(function(user) {
-              var user = firebase.auth().currentUser;
-              if (user) {
-                  //Now we can call the write function to create and set the scope into the fb
-                  writeUserData(user.uid, user.displayName, user.email);
-                  console.log(firebase.auth().currentUser.displayName);
-              } else {
-                  console.log("Login Failed");
-              }
-          });
-    };
+facebookService.login = function() {
+  var facebookProvider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(facebookProvider);
+    firebase.auth().onAuthStateChanged(function(user) {
+      var user = firebase.auth().currentUser;
+      if (user) {
+        //Now we can call the write function to create and set the scope into the fb
+        writeUserData(user.uid, user.displayName, user.email);
+        console.log(firebase.auth().currentUser.displayName);
+      } else {
+        console.log("Login Failed");
+      }
+    });
+  };
     return facebookService;
-});
-
+  });
 }]);
 
 
